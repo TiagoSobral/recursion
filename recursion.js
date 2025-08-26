d229c6134405e21f3a6456d32925b61892207a8b
Tiago Sobral <113380453+TiagoSobral@users.noreply.github.com>
2025-08-26 16:30:06 +0100

    (feat) iteration and recursion fibonacci

--- recursion.js
+++ recursion.js
@@ -1,1 +1,30 @@
 #!/usr/bin/node
+
+function fibs(number) {
+	let array = [];
+	for (let i = 0; i < number; i++) {
+		if (i < 2) {
+			array.push(i);
+		} else {
+			let fib = array[i - 1] + array[i - 2];
+			array.push(fib);
+		}
+	}
+
+	console.log(array);
+}
+
+function fibsRec(number) {
+	if (number === 1) {
+		return [0];
+	} else if (number === 2) {
+		return [0, 1];
+	} else {
+		let fibo = fibsRec(number - 1);
+		let next = fibo[fibo.length - 1] + fibo[fibo.length - 2];
+		fibo.push(next);
+		return fibo;
+	}
+}
+
+console.log(fibsRec(8));
